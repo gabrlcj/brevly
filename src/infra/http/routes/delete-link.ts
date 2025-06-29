22d09f426c5661abd794f59224de91571bb75510
gabrlcj <gabrielbittencourt57@gmail.com>
2025-06-29 14:06:02 -0300

    feat: adds delete link route and service

---
+++ src/infra/http/routes/delete-link.ts
@@ -0,0 +1,43 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { z } from 'zod/v4'
+import { deleteLink } from '@/services/delete-link'
+import { isRight, unwrapEither } from '@/shared/either'
+
+export const deleteLinkRoute: FastifyPluginAsyncZod = async server => {
+  server.delete(
+    '/links/:shortUrl',
+    {
+      schema: {
+        summary: 'Delete link by short URL',
+        params: z.object({
+          shortUrl: z.string(),
+        }),
+        response: {
+          204: z.object({}),
+          400: z.object({ message: z.string() }),
+          404: z.object({ message: z.string() }).describe('Link not found.'),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { shortUrl } = request.params
+
+      if (!shortUrl) {
+        return reply.status(400).send({ message: 'Short URL not provided.' })
+      }
+
+      const result = await deleteLink({ shortUrl })
+
+      if (isRight(result)) {
+        return reply.status(204).send({})
+      }
+
+      const error = unwrapEither(result)
+
+      switch (error.constructor.name) {
+        case 'NotFound':
+          return reply.status(404).send({ message: error.message })
+      }
+    }
+  )
+}
---
+++ src/services/delete-link.ts
@@ -0,0 +1,30 @@
+import { eq } from 'drizzle-orm'
+import { z } from 'zod/v4'
+import { db } from '@/infra/db'
+import { schema } from '@/infra/db/schemas'
+import { type Either, makeLeft, makeRight } from '@/shared/either'
+import { NotFound } from './errors/not-found'
+
+export const deleteLinkSchema = z.object({
+  shortUrl: z.string(),
+})
+
+type DeleteLinkSchemaInput = z.infer<typeof deleteLinkSchema>
+
+export async function deleteLink(
+  input: DeleteLinkSchemaInput
+): Promise<Either<NotFound, object>> {
+  const { shortUrl } = deleteLinkSchema.parse(input)
+
+  const existingLink = await db.query.links.findFirst({
+    where: eq(schema.links.shortUrl, shortUrl),
+  })
+
+  if (!existingLink) {
+    return makeLeft(new NotFound())
+  }
+
+  await db.delete(schema.links).where(eq(schema.links.shortUrl, shortUrl))
+
+  return makeRight({})
+}
