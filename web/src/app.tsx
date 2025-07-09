ceeee4549af7e5a5ae100a9a5ccaa2f1c9a8c31e
gabrlcj <gabrielbittencourt57@gmail.com>
2025-07-09 00:56:35 -0300

    feat: adds router provider and toast on app and main

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -1,7 +1,13 @@
+import { Toaster } from 'react-hot-toast'
+import { Home } from './components/home'
+
 export function App() {
 
   return (
-    <h1>Hello world</h1>
+    <main className='h-dvh max-w-[1240px] mx-auto px-5 py-8 md:py-0'>
+      <Home />
+      <Toaster />
+    </main>
   )
 }
 
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,11 @@
+import './index.css'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { App } from './app.tsx'
+import { RouterProvider } from 'react-router'
+import { router } from './routes.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
