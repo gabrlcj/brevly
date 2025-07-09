8d52eed5c17afab8d73b4e8779b46491ed8017ed
gabrlcj <gabrielbittencourt57@gmail.com>
2025-07-09 12:11:31 -0300

    chore: minor adjusts

--- web/src/components/home.tsx
+++ web/src/components/home.tsx
@@ -3,7 +3,7 @@ import { LinkList } from './link-list';
 
 export function Home() {
   return (
-    <div className='min-h-dvh flex flex-col md:justify-center md:items-center'>
+    <div className='h-full flex flex-col md:justify-center md:items-center'>
       <div className='w-full flex flex-col justify-center gap-5 md:flex-row'>
         <div className='w-full md:max-w-[440px]'>
           <LinkForm />
