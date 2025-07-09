7e86c4a57bfebe4cb1a3961d69d94bddd034564e
gabrlcj <gabrielbittencourt57@gmail.com>
2025-07-09 08:45:31 -0300

    refactor: adjust to use port from env

--- server/src/test/factories/make-link.ts
+++ server/src/test/factories/make-link.ts
@@ -2,6 +2,7 @@ import { randomUUID } from 'node:crypto'
 import type { InferInsertModel } from 'drizzle-orm'
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
+import { env } from '@/infra/env'
 
 export async function makeLink(
   overrides?: Partial<InferInsertModel<typeof schema.links>>
@@ -10,7 +11,7 @@ export async function makeLink(
     .insert(schema.links)
     .values({
       shortUrl: `${randomUUID()}`,
-      originalUrl: `http://localhost:3333/${randomUUID()}`,
+      originalUrl: `http://localhost:${env.PORT}/${randomUUID()}`,
       ...overrides,
     })
     .returning()
